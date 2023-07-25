2261c259c54d1327ceb4d571efe40ca754ccfbe6
RustamRu <kagapov.rustam@gmail.com>
2023-07-25 17:23:27 +0300

    Main component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
-// sr/App.vue
 <script>
 import Header from './components/Header/Header.vue';
-import StatList from './components/StatList/StatList.vue';
+import Main from './components/Main/Main.vue';
 import SideStat from './components/SideStat/SideStat.vue';
-import DistrictMap from "./assets/image/cfd-map.svg";
 
 export default {
   components: {
-    Header, StatList, SideStat, DistrictMap
+    Header, Main, SideStat
   }
 }
 </script>
 
 <template>
   <Header />
-  <h1 class="page-title">
-    <span class="accent">Центральная россия —</span> 1/3 экономики страны
-  </h1>
-  <StatList />
+  <Main />
   <SideStat />
-  <DistrictMap />
 </template>
 
 <style lang="scss">
---
+++ src/components/Main/Main.vue
@@ -0,0 +1,24 @@
+<script>
+import StatList from '../StatList/StatList.vue';
+import DistrictMap from "../../assets/image/cfd-map.svg";
+
+export default {
+  components: {
+    StatList, DistrictMap
+  }
+}
+</script>
+
+<template>
+  <main class="main">
+    <h1 class="main__title">
+      <span class="accent">Центральная россия —</span> 1/3 экономики страны
+    </h1>
+    <StatList />
+    <DistrictMap class="main__map"/>
+  </main>
+</template>
+
+<style lang="scss">
+@import "./Main.scss";
+</style>
